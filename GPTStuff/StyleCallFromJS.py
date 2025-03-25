7a2055a9ddf8f2bdbc3721df1cc109d8f797e8bf
Graham277 <graham.g.jarrett@gmail.com>
2025-03-25 19:37:15 -0400

    custom styles

---
+++ GPTStuff/StyleCallFromJS.py
@@ -0,0 +1,30 @@
+import ollama
+import re
+import sys
+
+# Get the prompt
+# filePath = '/home/dozer/GPTStuff/prompt.txt'
+# with open(filePath, 'r') as file:
+#    prompt = file.read()
+
+prompt = sys.argv[1]
+style = sys.argv[2]
+
+# Get the response from dozergpt
+combinedPrompt = 'Respond in less than 2 sentences. \nHere is the prompt from the user:\n' + prompt + "Here is the style/emotion that you must use in your response:" + style
+rawResponse = ollama.chat(model='DozerGPT', messages=[{'role': 'user', 'content': combinedPrompt}])
+weirdresponse = str(rawResponse)
+
+# Keep only what's inside the double quotes function
+def extract_content(weirdresponse):
+    content_regex = r"content=['\"](.*?)(?=['\"][,])"
+    only_text = re.findall(content_regex, weirdresponse)
+    joined_together = "".join(only_text)
+    response = joined_together.replace(r'\n', '\n')  # This will make sure \n is interpreted as a newline
+    return response
+response = extract_content(weirdresponse)
+
+# Put the data into the file 
+filePath = '/home/dozer/GPTStuff/response.txt'
+with open(filePath, "w") as file:
+    file.write(response)
